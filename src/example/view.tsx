cfe89dd52db852f88753d99421ea0fac4394672d
h4ppye <h4ppye@gmail.com>
2023-04-26 22:21:30 +0100

    minor fix

--- src/example/view.tsx
+++ src/example/view.tsx
@@ -1,77 +1,73 @@
 import { Vvm } from "../svvm";
 import {
-    AddTodoViewModel,
-    createAddTodoViewModel,
-    createTodoItemViewModel,
-    createTodoListViewModel,
-    TodoItemViewModel,
-    TodoListViewModel,
+  AddTodoViewModel,
+  createAddTodoViewModel,
+  createTodoItemViewModel,
+  createTodoListViewModel,
+  TodoItemViewModel,
+  TodoListViewModel,
 } from "./viewModel";
 
 function AddTodoView(vm: AddTodoViewModel) {
-    return (
-        <div>
-            <input
-                type="text"
-                value={vm.addTodoText}
-                onChange={e => vm.setAddTodoText(e.target.value)}
-            />
-            <button
-                onClick={() => vm.addTodo()}
-                disabled={vm.addTodoText === ""}
-            >
-                +
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="text"
+        value={vm.addTodoText}
+        onChange={(e) => vm.setAddTodoText(e.target.value)}
+      />
+      <button onClick={() => vm.addTodo()} disabled={vm.addTodoText === ""}>
+        +
+      </button>
+    </div>
+  );
 }
 
 function TodoItemView(vm: TodoItemViewModel) {
-    return (
-        <div key={vm.item.key}>
-            <input
-                type="checkbox"
-                checked={vm.item.completed}
-                onChange={() => vm.toggleComplete()}
-            />
-            <span
-                style={{
-                    textDecoration: vm.item.completed
-                        ? "line-through"
-                        : undefined,
-                }}
-            >
-                {vm.item.description}
-            </span>
-            <button onClick={() => vm.delete()}>x</button>
-        </div>
-    );
+  return (
+    <div key={vm.item.key}>
+      <input
+        type="checkbox"
+        checked={vm.item.completed}
+        onChange={() => vm.toggleComplete()}
+      />
+      <span
+        style={{
+          textDecoration: vm.item.completed ? "line-through" : undefined,
+        }}
+      >
+        {vm.item.description}
+      </span>
+      <button onClick={() => vm.delete()}>x</button>
+    </div>
+  );
 }
 
 function TodoListView(vm: TodoListViewModel) {
-    return (
-        <div>
-            {vm.todoList.map(item => (
-                <Vvm<TodoItemViewModel>
-                    view={TodoItemView}
-                    viewModel={createTodoItemViewModel(item)}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {vm.todoList.map((item) => (
+        <Vvm<TodoItemViewModel>
+          key={item.key}
+          view={TodoItemView}
+          viewModel={createTodoItemViewModel(item)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export function App() {
-    return (
-        <div>
-            <Vvm<AddTodoViewModel>
-                view={AddTodoView}
-                viewModel={createAddTodoViewModel()}
-            />
-            <Vvm<TodoListViewModel>
-                view={TodoListView}
-                viewModel={createTodoListViewModel()}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Vvm<AddTodoViewModel>
+        view={AddTodoView}
+        viewModel={createAddTodoViewModel()}
+      />
+      <Vvm<TodoListViewModel>
+        view={TodoListView}
+        viewModel={createTodoListViewModel()}
+      />
+    </div>
+  );
 }
